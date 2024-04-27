c631c24a2c8300bf6eff3333518bf2b8a3a062c1
lemontea915461 <lemontea17159@outlook.com>
2024-04-27 22:24:14 +0800

    4.27

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -29,14 +29,15 @@ enum Fruit {
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     let fruit_kinds = vec![
-        Fruit::Apple,
+        //Fruit::Apple,
         Fruit::Banana,
-        Fruit::Mango,
-        Fruit::Lychee,
+        //Fruit::Mango,
+        //Fruit::Lychee,
         Fruit::Pineapple,
     ];
 
     for fruit in fruit_kinds {
+        basket.entry(fruit).or_insert(1);
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
